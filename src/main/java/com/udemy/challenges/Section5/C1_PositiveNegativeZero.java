779e105392a780ed6f213826fa641764364b6f78
Musa Hoosain <musahoosain@gmail.com>
2024-11-29 15:17:35 -0500

    minor changes

--- src/main/java/com/udemy/challenges/Section5/C1_PositiveNegativeZero.java
+++ src/main/java/com/udemy/challenges/Section5/C1_PositiveNegativeZero.java
@@ -1,5 +1,25 @@
 package com.udemy.challenges.Section5;
 
+/*Positive, Negative or Zero
+Write a method called checkNumber with an int parameter named number.
+
+
+
+The method should not return any value, and it needs to print out:
+
+    - "positive" if the parameter number is > 0
+
+    - "negative" if the parameter number is < 0
+
+    - "zero" if the parameter number is equal to 0
+
+
+
+NOTE:  The checkNumber method needs to be defined as public static like we have been doing so far in the course.
+
+NOTE:  Do not add a main method to your solution code!*/
+
+
 public class C1_PositiveNegativeZero {
 
     public static void main(String[] args) {
